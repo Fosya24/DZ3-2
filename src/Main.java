b7c40379a3ab8c71252644236d81e836b2d63218
Fosya <napka1121@mail.ru>
2026-04-22 15:04:57 +0700

    change main

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         BmiService service = new BmiService();
         double heightM = 1.87;
         int weightKG = 98;
-        int Bmi = service.calculate((int) weightKG, heightM);
+        int Bmi = service.calculate(weightKG, heightM);
         System.out.println(Bmi);
     }
 }
